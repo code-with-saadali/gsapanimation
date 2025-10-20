3de9b944d4b436acac8351600f1ae0889c18efb1
code-with-saadali <saadali786000786@gmail.com>
2025-10-20 19:54:55 +0500

    first commit

--- src/app/_components/Example10.tsx
+++ src/app/_components/Example10.tsx
@@ -46,11 +46,7 @@ const CenterImage = () => {
     [0, SECTION_HEIGHT + 500],
     ["170%", "100%"]
   );
-  const opacity = useTransform(
-    scrollY,
-    [SECTION_HEIGHT, SECTION_HEIGHT + 500],
-    [1, 0]
-  );
+ 
 
   return (
     <motion.div
